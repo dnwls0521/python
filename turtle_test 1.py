906449e81669ce816ea02dabe1ad200270d5f5d9
dnwls0521 <162937232+dnwls0521@users.noreply.github.com>
2024-03-22 14:14:44 +0900

    Add files via upload

---
+++ turtle_test 1.py
@@ -0,0 +1,21 @@
+# 터틀 별그리기
+
+import turtle
+turtle.shape("turtle")
+
+turtle.color("blue")
+turtle.begin_fill()
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.left(144)
+turtle.forward(100)
+turtle.end_fill()
+
+
